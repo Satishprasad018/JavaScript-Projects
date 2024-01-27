f41285804c5a70c4c05ee5f293ff7e11a5647c32
Satish <prasadsatish333@gmail.com>
2024-01-27 15:01:29 +0530

    Second JavaScript Project

---
+++ 02_bmiCalculator/bmiCalc.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event_1) {
+  event_1.preventDefault();
+
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = "please give a valid height";
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = "please give a valid weight";
+  }else {
+    const bmi = (weight /((height*height)/10000)).toFixed(2);
+    if(bmi < 18.6){
+         results.innerHTML = `<span>${bmi} Under Weight</span>`
+    }else if( bmi > 24.9){
+         results.innerHTML = `<span>${bmi} Over Weight</span>`
+    }else{
+         results.innerHTML = `<span>${bmi} Normal Weight</span>`
+    }
+    
+  }
+});
